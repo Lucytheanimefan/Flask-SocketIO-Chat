4b1b240641dafacafc78f5f46bc0f4dfc05d86d2
Lucy Zhang <spothorse9.lucy@gmail.com>
2016-12-01 19:47:28 -0500

    Added procfile

--- chat.py
+++ chat.py
@@ -1,7 +1,8 @@
 #!/bin/env python
 from app import create_app, socketio
 
-app = create_app(debug=True)
+port = int(os.environ.get("PORT", 5000))
+app = create_app(host='0.0.0.0', port=port)
 
 if __name__ == '__main__':
     socketio.run(app)
